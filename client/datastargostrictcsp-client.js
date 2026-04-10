46d319cc53f691d15a14ea263378d15a8859969d
Alex Drummond <a.d.drummond@gmail.com>
2026-04-10 12:19:25 +0100

    tweak comment

--- client/datastargostrictcsp-client.js
+++ client/datastargostrictcsp-client.js
@@ -4,8 +4,10 @@
   //
   // Tracks per-render nonces from precompile scripts. We reject expression
   // invocations on elements whose data-ds-nonce is not in the filter.
-  // This is primarily a protection against server-side injection, but also
-  // adds an additional layer of protection against client-side injection.
+  // This is primarily a protection against server-side injection (handling
+  // the case where an attacker has injected an existing precompiled attribute),
+  // but it also adds an additional layer of protection against client-side
+  // injection.
   //
   // Two rotating bloom filters, each m=2^14 bits (2 KB), k=9.
   // Insertions go into `cur`; after 1000 insertions `cur` and `old` swap and
